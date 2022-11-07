28b8caa8c4439d3fe38558f142051725762df9d4
Manuel Šarić <manujelko@gmail.com>
2022-11-07 22:27:23 +0100

    Use winfo_children

--- app.py
+++ app.py
@@ -32,14 +32,17 @@ feet_label = ttk.Label(main, text="Feet:")
 feet_display = ttk.Label(main , textvariable=feet_value)
 calc_button = ttk.Button(main, text="Calculate", command=calculate_feet)
 
-meters_label.grid(column=0, row=0, sticky="W", padx=15, pady=15)
-meters_input.grid(column=1, row=0, sticky="EW", padx=15, pady=15)
+meters_label.grid(column=0, row=0, sticky="W")
+meters_input.grid(column=1, row=0, sticky="EW")
 meters_input.focus()
 
-feet_label.grid(column=0, row=1, stick="W", padx=15, pady=15)
-feet_display.grid(column=1, row=1, sticky="EW", padx=15, pady=15)
+feet_label.grid(column=0, row=1, stick="W")
+feet_display.grid(column=1, row=1, sticky="EW")
 
-calc_button.grid(column=0, row=2, columnspan=2, sticky="EW", padx=15, pady=15)
+calc_button.grid(column=0, row=2, columnspan=2, sticky="EW")
+
+for child in main.winfo_children():
+    child.grid_configure(padx=15, pady=15)
 
 root.bind("<Return>", calculate_feet)
 root.bind("<KP_Enter>", calculate_feet)
