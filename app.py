fd5096bcc0cad5fa6d896d5cab12449d672276c8
Manuel Šarić <manujelko@gmail.com>
2022-11-08 17:41:37 +0100

    Object oriented distance converter

--- app.py
+++ app.py
@@ -3,51 +3,54 @@ import tkinter.font as font
 from tkinter import ttk
 
 
-root = tk.Tk()
-root.title("Distance converter")
-
-font.nametofont("TkDefaultFont").configure(size=15)
-
-meters_value = tk.StringVar()
-feet_value = tk.StringVar(value="Feet shown here")
-
-
-def calculate_feet(*args):
-    try:
-        meters = float(meters_value.get())
-        feet = meters * 3.28084
-        feet_value.set(f"{feet:.3f}")
-    except ValueError:
-        pass
+class DistanceConverter(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title("Distance converter")
+        frame = MetersToFeet(self, padding=(60, 30))
+        frame.grid()
+        self.bind("<Return>", frame.calculate_feet)
+        self.bind("<KP_Enter>", frame.calculate_feet)
 
 
-root.columnconfigure(0, weight=1)
+class MetersToFeet(ttk.Frame):
+    def __init__(self, container, **kwargs):
+        super().__init__(container, **kwargs)
+        
+        self.feet_value = tk.StringVar()
+        self.meters_value = tk.StringVar()
 
-main = ttk.Frame(root, padding=(30, 50))
-main.grid()
+        meters_label = ttk.Label(self, text="Meters:")
+        meters_input = ttk.Entry(self, width=10, textvariable=self.meters_value, font=("Segoe UI", 15))
+        feet_label = ttk.Label(self, text="Feet:")
+        feet_display = ttk.Label(self, textvariable=self.feet_value)
+        calc_button = ttk.Button(self, text="Calculate", command=self.calculate_feet)
 
-meters_label = ttk.Label(main, text="Meters:")
-meters_input = ttk.Entry(main, width=10, textvariable=meters_value, font=("Segoe UI", 15))
-feet_label = ttk.Label(main, text="Feet:")
-feet_display = ttk.Label(main , textvariable=feet_value)
-calc_button = ttk.Button(main, text="Calculate", command=calculate_feet)
+        meters_label.grid(column=0, row=0, sticky="W")
+        meters_input.grid(column=1, row=0, sticky="EW")
+        meters_input.focus()
 
-meters_label.grid(column=0, row=0, sticky="W")
-meters_input.grid(column=1, row=0, sticky="EW")
-meters_input.focus()
+        feet_label.grid(column=0, row=1, sticky="W")
+        feet_display.grid(column=1, row=1, sticky="EW")
 
-feet_label.grid(column=0, row=1, stick="W")
-feet_display.grid(column=1, row=1, sticky="EW")
+        calc_button.grid(column=0, row=2, columnspan=2, sticky="EW")
 
-calc_button.grid(column=0, row=2, columnspan=2, sticky="EW")
+        for child in self.winfo_children():
+            child.grid_configure(padx=15, pady=15)
 
-for child in main.winfo_children():
-    child.grid_configure(padx=15, pady=15)
 
-root.bind("<Return>", calculate_feet)
-root.bind("<KP_Enter>", calculate_feet)
+    def calculate_feet(self, *args):
+        try:
+            meters = float(self.meters_value.get())
+            feet = meters * 3.28084
+            self.feet_value.set(f"{feet:.3f}")
+        except ValueError:
+            pass
 
 
 if __name__ == "__main__":
+    root = DistanceConverter()
+    font.nametofont("TkDefaultFont").configure(size=15)
+    root.columnconfigure(0, weight=1)
     root.mainloop()
  
